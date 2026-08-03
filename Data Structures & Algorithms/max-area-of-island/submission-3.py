69a313a1f202a009562343de12072a741fc3896d
William Duong <william.duong2023+github@gmail.com>
2026-08-03 18:15:13 +1000

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.py
@@ -0,0 +1,45 @@
+WATER = 0
+LAND = 1
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        best = 0
+        seen = set()
+
+        for i, line in enumerate(grid):
+            for j, cell in enumerate(line):
+                if grid[i][j] != LAND:
+                    continue
+
+                queue = deque()
+                queue.append((i, j))
+                area = 0
+
+                while queue:
+                    r, c = queue.popleft()
+                    seen.add((r, c))
+                    area += 1
+
+                    # print(r, c)
+                    # print(queue)
+                    # print(seen)
+                    # print()
+
+                    for dr, dc in DIRECTIONS:
+                        x, y = r + dr, c + dc
+
+                        if (
+                            (x, y) in seen
+                            or not (0 <= x <= len(grid) - 1 and 0 <= y <= len(grid[0]) - 1)
+                            or grid[x][y] != LAND
+                        ):
+                            continue
+
+                        queue.append((x, y))
+
+                best = max(best, area)
+
+        return best
